83e4fdfabb6a94be49682b95d8f62b0600a5816c
flameboy <shuvogit9932github@gmail.com>
2026-08-31 20:56:28 +0530

    feat: login and signup updated

--- src/views/pages/AuthView.jsx
+++ src/views/pages/AuthView.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import Navbar from '../../components/Navbar/Navbar';
+import Hero2 from '../../components/Hero/Hero2';
 import LoginSignup from '../../components/Auth/LoginSignup';
+import Footer from '../../components/Footer/Footer';
 
 export function AuthView() {
   return (
-    <div className="auth-page">
-      <LoginSignup />
+    <div className="auth-page-wrapper">
+      <Navbar />
+      <Hero2 title="Guest Portal" subtitle="Log in or create an account to manage your luxury stays & dining" />
+      <main className="auth-main-content">
+        <LoginSignup />
+      </main>
+      <Footer />
     </div>
   );
 }
